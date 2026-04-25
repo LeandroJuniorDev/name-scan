d2efd1f21a445b9bd728b3b7eee9676b5ee5a536
Leandro da Silva Junior <leandrojunior.dev@gmail.com>
2026-04-25 20:33:25 -0300

    build: scaffold NameScan Blazor solution

---
+++ NameScan/Program.cs
@@ -0,0 +1,29 @@
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+builder.Services.AddMemoryCache();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<NameScan.Components.App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
+public partial class Program;
